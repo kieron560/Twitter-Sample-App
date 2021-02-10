b14138bd5475a19273d0ef0a0243b94ea35beb8b
kieron560 <kieron560@gmail.com>
2021-02-10 10:13:27 +0000

    Attempt to fix bugs

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -24,37 +24,37 @@ class PasswordResetsController < ApplicationController
   
   def update
     if params[:user][:password].empty?                  # Case (3)
-    @user.errors.add(:password, "can't be empty")
-    render 'edit'    
+      @user.errors.add(:password, "can't be empty")
+      render 'edit'
     elsif @user.update(user_params)                     # Case (4)
-       reset_session
+      reset_session
       log_in @user
       flash[:success] = "Password has been reset."
       redirect_to @user
-    else      
+    else
       render 'edit'                                     # Case (2)
-    end  
+    end
   end
   
   private
-
+  
     def user_params
       params.require(:user).permit(:password, :password_confirmation)
     end
     
-    def get_user      
+    def get_user
       @user = User.find_by(email: params[:email])
     end
 
     # Confirms a valid user.
-    def valid_user      
+    def valid_user
       unless (@user && @user.activated? &&
-      @user.authenticated?(:reset, params[:id]))
-      redirect_to root_url
+              @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
       end
     end
 
-# Checks expiration of reset tok    
+# Checks expiration of reset token.     
     def check_expiration
       if @user.password_reset_expired?
         flash[:danger] = "Password reset has expired."
