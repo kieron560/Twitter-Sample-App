744eec3b6482d4131fb37ae9e4783d974401f023
kieron560 <kieron560@gmail.com>
2021-02-10 02:42:28 +0000

    Add Account Activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,12 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  host = 'localhost:3000'  
+  # Use this on the cloud IDE.
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  # Use this if developing on localhost.
+  # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   config.action_mailer.perform_caching = false
 
@@ -75,4 +81,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   # Allow connections to local server.
   config.hosts.clear
+  
+  
 end
